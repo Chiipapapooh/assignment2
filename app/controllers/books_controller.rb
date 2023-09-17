f7116c7ee3f31a8678e1e51c3129b572d8c87515
Chiipapapooh <143480503+Chiipapapooh@users.noreply.github.com>
2023-09-17 12:53:42 +0000

    resubmit1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,17 +14,22 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-    redirect_to '/books'
+      flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
     else
     @books = Book.all
     render :index
-    end
+    end 
   end 
   
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to '/books' 
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id)
+    else
+    render :edit
+    end 
   end
   
   def edit
@@ -33,13 +38,14 @@ class BooksController < ApplicationController
  
   def destroy
     book = Book.find(params[:id])  
-    book.destroy  
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'  
   end
   
   private
+  
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
-end
+end 
